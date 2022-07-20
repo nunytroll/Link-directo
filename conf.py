201cc5ad740e6fc9598261df1b3104287fcf5ffb
nunytroll <98285312+nunytroll@users.noreply.github.com>
2022-07-20 23:44:05 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
-api_hash = "0c780f6db3124fafbdc3dcde437e6f21"
-api_id = 16536294
-bot_token = "5303873066:AAHoGX4eiQHmmZrOMzM47ROWcQ47lrcfSxY"
-bot_admin_group = -1001505005021
+api_hash = "000d66c8f9c490c81dd64d7a48f55d1d"
+api_id = 16643256
+bot_token = "5451934593:AAEtEv40xTXXhuAdtB2AUvuXH5jsv3KF_20"
+bot_admin_group = -5269238222
